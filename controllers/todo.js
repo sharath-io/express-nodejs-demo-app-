22bb8a2f3fff2248de8112b773a0778bad59ebab
Sharath <sharath.esr@gmail.com>
2024-09-27 18:07:15 +0530

    feat: Impelement update and delete routes

--- controllers/todo.js
+++ controllers/todo.js
@@ -1,26 +1,65 @@
 const Todo = require('../models/todo')
 
-const getTodos = async (req,res) =>{
-try{
-    const todos = await Todo.find({})
-    res.send(todos);
-} catch (error){
-    console.log(error);
-    res.status(500).send();
-}
+const getTodos = async (req, res) => {
+    try {
+        const todos = await Todo.find({})
+        res.send(todos);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send();
+    }
 };
 
-const addTodo = async (req,res) =>{
-    try{
-     const todo = new Todo(req.body);
-     await todo.save();
-     res.status(201).send(todo);
-    }catch(error){
+const addTodo = async (req, res) => {
+    try {
+        const todo = new Todo(req.body);
+        await todo.save();
+        res.status(201).send(todo);
+    } catch (error) {
         res.status(500).send();
-    }   
-}   
+    }
+}
 
-module.exports ={
+
+const updateTodo = async (req, res) => {
+    try {
+        const isValidId = await Todo.findById(req.params.id);
+        if(!isValidId){
+            return res.status(400).send('Please enter valid Todo Id')
+        }
+        const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, req.body, {
+            new: true
+        })
+        res.send(updatedTodo);
+    } catch (e) {
+        res.status(500).send();
+    }
+}
+
+
+const deleteTodo = async (req,res) =>{
+    try {
+        // if document does not exist => retuns  null
+        const isValidId = await Todo.findById(req.params.id);
+        if(!isValidId){
+            return res.status(400).send('Please enter valid Todo Id')
+        }
+        const deletedTodo = await Todo.findByIdAndDelete(req.params.id);
+        res.send(deletedTodo);    
+    } catch (e) {
+        console.log(e); 
+        res.status(500).send();
+    }
+}
+
+
+
+
+
+
+module.exports = {
     getTodos,
-    addTodo
+    addTodo,
+    updateTodo,
+    deleteTodo
 }
--- routes/todo.js
+++ routes/todo.js
@@ -1,10 +1,14 @@
 const  express = require('express')
-const { addTodo, getTodos } = require('../controllers/todo');
+const { addTodo, getTodos, updateTodo, deleteTodo } = require('../controllers/todo');
 
 const Router = express.Router();
 
 Router.get('/', getTodos);
 
-Router.post('/', addTodo)
+Router.post('/', addTodo);
+
+Router.patch('/:id', updateTodo);
+
+Router.delete('/:id', deleteTodo);
 
 module.exports = Router;
